682de3d730b18574f1cb7907a28d9d1bc0aa5e37
Taylor Devereaux <taylor.devereaux@tks.ca>
2021-05-02 11:53:36 -0300

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace text_adventure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
